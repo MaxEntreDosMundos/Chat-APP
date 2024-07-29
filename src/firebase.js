7e4ae55911300b6c811c58b003ed82f07f9eff5b
Max Ricardo ap <maxriappa@gmail.com>
2024-07-28 19:36:40 -0500

    subiendo React-Chat al repositorio

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMKycjo3oINbFGtMKx5KsADkfBas3OUNA",
+  authDomain: "chat-react-b50a1.firebaseapp.com",
+  projectId: "chat-react-b50a1",
+  storageBucket: "chat-react-b50a1.appspot.com",
+  messagingSenderId: "247151588956",
+  appId: "1:247151588956:web:b0aecec2144a6c07be5899"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
