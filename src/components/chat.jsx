ea297151793f9d98c57bf7b011ef6ce0cf70fc4f
Max Ricardo ap <maxriappa@gmail.com>
2024-07-30 22:50:18 -0500

    subiendo estilo

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,43 +1,36 @@
 import { useEffect, useState } from "react";
-import {auth, db} from "../firebase";
-import { query,collection,orderBy,onSnapshot, QuerySnapshot } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
 import Message from "./Message"; // Ensure this import is correctly pointing to your Message component file
 
+const Chat = () => {
+    const [messages, setMessages] = useState([]);
 
-const Chat = () =>{
+    useEffect(() => {
+        const newQuery = query(collection(db, 'messages'), orderBy('timestamp'));
 
-    const [messages, setMessage]= useState([]);
-
-    useEffect(()=>{
-        const newQuery = query(collection(db,'messages'), orderBy('timestamp'));
-
-        const unsubscribe = onSnapshot(newQuery,(QuerySnapshot)=>{
+        const unsubscribe = onSnapshot(newQuery, (QuerySnapshot) => {
             let currentMessages = [];
-            QuerySnapshot.forEach(item=>{
-                currentMessages.push({ content: item.data(), id: item.id })
+            QuerySnapshot.forEach(item => {
+                currentMessages.push({ content: item.data(), id: item.id });
                 console.log(item.data());
-            })
-            setMessage(currentMessages)
-        })
+            });
+            setMessages(currentMessages);
+        });
+
         return unsubscribe;
-    }
-    )
+    }, []); // Add an empty dependency array to run useEffect only once on mount
 
-    return(
-
+    return (
         <section className="chat-content">
-            
-            {
-                messages && messages.map(item => (
-                    <Message
-                        key={item.id}
-                        messages={item.content}
-                    />
-                ))
-            }
-
-            <h4>Este es un mensaje</h4>
+            {messages && messages.map(item => (
+                <Message
+                    key={item.id}
+                    messages={item.content}
+                />
+            ))}
         </section>
     );
-}
-export default Chat
+};
+
+export default Chat;
